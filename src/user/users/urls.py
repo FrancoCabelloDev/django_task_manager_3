f11fd7ffb45fa0941337040c7594e45a69bfbd48
chebas77 <manuel.rodriguez.j@tecsup.edu.pe>
2025-03-31 17:40:17 -0500

    Subiendo cambios a la rama ManuelRodriguez

---
+++ src/user/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import register, user_login, user_logout
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+]
